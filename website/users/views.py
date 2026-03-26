75258a76996444725357eab61a48312a059d4ea7
thomas-m33 <tmiraziz@parrastu.catholic.edu.au>
2026-03-26 19:05:38 +1100

    Profile customisation + styling bugfixes

--- website/users/views.py
+++ website/users/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
-import logging
 
 def register(request):
     if request.method == 'POST':
@@ -18,9 +17,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
-        'u-form': u_form
+        'u_form': u_form,
+        'p_form': p_form
     }
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html', context)
 
